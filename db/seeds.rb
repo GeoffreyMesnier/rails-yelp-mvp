8f42da0f7ac26b2aafea986de8602e96171fa441
geoffrey mesnier <mesniergeoffrey@gmail.com>
2021-02-24 17:12:57 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creation de 5 restaurants"
+
+entrecote = Restaurant.new(name: "entrecote", category: "french", address: "2 rue des lilas")
+entrecote.save!
+quick = Restaurant.new(name: "quick", category: "belgian", address: "2 place du commerce")
+quick.save!
+grande_muraille = Restaurant.new(name: "grande muraille", category: "japanese", address: "rue des champs")
+grande_muraille.save!
+sushi = Restaurant.new(name: "sushi shop", category: "chinese", address: "rues des ponts")
+sushi.save!
+pizza = Restaurant.new(name: "pizza etna", category: "italian", address: "rues des chevaux")
+pizza.save!
+
+
+
+puts "Fin de la creation de 5 restaurant"
